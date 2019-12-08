19b2da89f9f39b78fc5666358658a88063a0ce02
Simon Rother <s9809075@mail.zih.tu-dresden.de>
2019-12-09 00:53:09 +0100

    tryed fix hover bug

--- con3/js/flipcard.js
+++ con3/js/flipcard.js
@@ -1,16 +1,21 @@
 var flipped = false;
+var start = Date.now();
 
 function flipBack(obj) {
-    if (!flipped){
+    if (!flipped && Date.now() - start > 50){
+        console.log("triggered back");
         obj.style.transform = "rotateY(180deg)";
         flipped = true;
+        start = Date.now();
     }
         
 }
 
 function flipFront(obj) {
-    if (flipped) {
+    if (flipped && Date.now() - start > 50) {
+        console.log("triggered front");
         obj.style.transform = "rotateY(0deg)";
         flipped = false;
+        start = Date.now();
     }  
 }
