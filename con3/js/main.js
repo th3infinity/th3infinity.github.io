d441dfe10d6766b0329240b01bc7901fbe1f3ba5
Simon <simon-paul-rother@gmx.de>
2019-12-15 12:54:56 +0100

    back to top button pos update

--- con3/js/main.js
+++ con3/js/main.js
@@ -1,39 +1,39 @@
-$(document).ready(function(){
-	
-
-	/*  Foundation Init    */
-	$(document).foundation();
+$(document).ready(function() {
 
 
+    /*  Foundation Init    */
+    $(document).foundation();
 
-	/*  carousel Init    */
-	$('#carousel').carouFredSel({
-	 	width : '670',
-	 	pagination  : ".pagination",
-	 	responsive : true,
-	 	scroll :{
-			 fx : 'fade',
-			 pauseOnHover: true,
-			 duration: 800
-	 	},
-	 	items :{
-	 		visible : 1,
-			 width : '670'
-	 	},
-		swipe: {
-			onMouse: true,
-			onTouch: true
-		}
+
+
+    /*  carousel Init    */
+    $('#carousel').carouFredSel({
+        width: '670',
+        pagination: ".pagination",
+        responsive: true,
+        scroll: {
+            fx: 'fade',
+            pauseOnHover: true,
+            duration: 800
+        },
+        items: {
+            visible: 1,
+            width: '670'
+        },
+        swipe: {
+            onMouse: true,
+            onTouch: true
+        }
     });
 
 
 
 
 
-	/*    Mean navigation menu scroll to    */
-    $('#mean_nav ul li a').click(function(e){
-    	e.preventDefault();
-    	scrollTo($(this).attr('href'), 900, 'easeInOutCubic');
+    /*    Mean navigation menu scroll to    */
+    $('#mean_nav ul li a').click(function(e) {
+        e.preventDefault();
+        scrollTo($(this).attr('href'), 900, 'easeInOutCubic');
     });
 
 
@@ -41,24 +41,22 @@ $(document).ready(function(){
     /*    Back to top button    */
     var back_top = $('#back_top');
 
-    back_top.click(function(e){
-    	e.preventDefault();
-    	scrollTo(0, 900, 'easeInOutCubic');
-    	
+    back_top.click(function(e) {
+        e.preventDefault();
+        scrollTo(0, 900, 'easeInOutCubic');
+
     });
 
-    function scrollTo(target, speed, ease){
-    	$(window).scrollTo(target, speed, {easing:ease});
+    function scrollTo(target, speed, ease) {
+        $(window).scrollTo(target, speed, { easing: ease });
     }
 
-    $(window).on('scroll', function(){    
-	    if($(this).scrollTop()>749)
-	    {
-	    	back_top.stop().animate({opacity : 1}, 250);
-	    }else
-	    {
-	    	back_top.stop().animate({opacity : 0}, 250);	    
-	    }   
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 2849) {
+            back_top.stop().animate({ opacity: 1 }, 250);
+        } else {
+            back_top.stop().animate({ opacity: 0 }, 250);
+        }
     });
 
 });
